a6528ee30e0ad001b7d0cd1db7fb6379ceaee811
Nitish Beck Bhagat <nitishbeckbhagat@gmail.com>
2023-05-07 19:22:25 +0530

    newly updated

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
 import json
 import pickle
-from flask import Flask,jsonify,request,app,url_for,render_template
+from flask import Flask, Response,jsonify,request,app,url_for,render_template
 
 import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
-dtreemodel = pickle.load(open('mymodel.pkl','rb'))
-scalar = pickle.load(open('scaler.pkl','rb'))
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods= ['POST'])
-def predict_api():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output = dtreemodel.predict(new_data)
-    print(output)
-    return jsonify(output)
 
-if __name__ =='__main__':
-    app.run(debug= True)
-    
+@app.route("/predict_api", methods=['POST'])
+def predict_api():
+    try:
+        if request.json['data'] is not None:
+            data = request.json['data']
+            print('data is:     ', data)
+            res = predict_log(data)
+            print('result is        ',res)
+            return Response(res)
+    except ValueError:
+        return Response("Value not found")
+    except Exception as e:
+        print('exception is   ',e)
+        return Response(e)
+
+def predict_log(dict_pred):
+    with open("scaler.pkl", 'rb') as f:
+        scalar = pickle.load(f)
+
+    with open("mymodel.pkl", 'rb') as f:
+        model = pickle.load(f)
+
+    data_df = pd.DataFrame(dict_pred,index=[1,])
+    scaled_data = scalar.transform(data_df)
+    predict = model.predict(scaled_data)
+    if predict[0] == 3:
+        result = 'Bad'
+    elif predict[0] == 4 :
+        result = 'Below Average'
+    elif predict[0]==5:
+        result = 'Average'
+    elif predict[0] == 6:
+        result = 'Good'
+    elif predict[0] == 7:
+        result = 'Very Good'
+    else :
+        result = 'Excellent'
+
+    return result
+if __name__ == "__main__":
+    app.run(debug=True)
